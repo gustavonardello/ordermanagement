ba653148b6bee65068b30fac63bc1e6bec142e45
Victor Nardello <victordrum17@gmail.com>
2022-09-24 16:49:34 -0300

    Corrigindo erros do projeto, limpando comentarios e imports de classes

--- src/main/java/br/com/ordermanagement/ordermanagement/domain/MonetaryDomain.java
+++ src/main/java/br/com/ordermanagement/ordermanagement/domain/MonetaryDomain.java
@@ -1,9 +1,8 @@
 package br.com.ordermanagement.ordermanagement.domain;
 
 import java.math.BigDecimal;
+
 import javax.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 public class MonetaryDomain 
@@ -11,4 +10,23 @@ public class MonetaryDomain
     public BigDecimal value;
     public BigDecimal cashback;
     public BigDecimal balance;
+    
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public BigDecimal getCashback() {
+		return cashback;
+	}
+	public void setCashback(BigDecimal cashback) {
+		this.cashback = cashback;
+	}
+	public BigDecimal getBalance() {
+		return balance;
+	}
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
 }
